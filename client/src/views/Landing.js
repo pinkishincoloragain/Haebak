2b84fe67bc3fd3021c66d2f0dd3514180bb121d0
bsu <bsu0541@naver.com>
2021-07-20 21:15:07 +0900

    Make LandingLogin Comp

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { Button, TextField, Link, Paper, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import { Paper, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Login from "../components/LandingLogin";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,22 +14,6 @@ const useStyles = makeStyles((theme) => ({
         alignContent: 'space-around',
         justifyContent: 'center',
     },
-    paper: {
-        margin: theme.spacing(6, 3),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    up: {
-        textAlign: 'center',
-    },
     title: {
         margin: theme.spacing(0, 0, 0, 6),
         display: 'flex',
@@ -38,76 +24,11 @@ const useStyles = makeStyles((theme) => ({
 
 function Landing() {
     const classes = useStyles();
-    const init = {
-        email: "",
-        passwd: "",
-      };
-      
-    const [inputs, setInputs] = useState(init);
-
-    const onChange = (e) => {
-        e.preventDefault();
-        const { name, value } = e.target;
     
-        setInputs({
-          ...inputs,
-          [name]: value,
-        });
-      };
-
-    const handleSubmit = () => {
-        console.log(inputs)
-    }
-
     return (
         <Grid container component="main" className={classes.root}>
             <Grid item xs={8} sm={8} md={3} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                        LOGIN
-                    </Typography>
-                    <form className={classes.form} onSubmit={handleSubmit} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email Address"
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={inputs.email}
-                            autoFocus
-                            onChange={onChange}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Password"
-                            type="password"
-                            id="passwd"
-                            name="passwd"
-                            value={inputs.passwd}
-                            onChange={onChange}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            LOGIN
-                        </Button>
-                        <Grid className={classes.up}>
-                            <Link href="#" variant="body2">
-                                {"Join now"}
-                            </Link>
-                        </Grid>
-                    </form>
-                </div>
+                <Login />
             </Grid>
             <Typography component="h1" variant="h2" className={classes.title}>
                 KNU&nbsp;
