0fc6630d3ec7fe7b37eac67f18c10d44e0c0c431
pinkishincoloragain <smb1103@gmail.com>
2021-07-21 05:15:46 +0900

    now working on recording

--- client/src/components/GetRecord.js
+++ client/src/components/GetRecord.js
@@ -3,10 +3,12 @@ import { dbService } from "../firebase";
 import Record from "./Record";
 
 const GetRecord = ({ userObj }) => {
-  // main data
+  // test for record (text version)
   const [record, setRecord] = useState("");
-  // list of data
+  // list of data (text version)
   const [records, setRecords] = useState([]);
+  // record file
+  const [recordData, setRecordData] = useState(null);
 
   useEffect(() => {
     // onSnapshot -> detect change from DB
@@ -25,16 +27,16 @@ const GetRecord = ({ userObj }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // insert data to database
-    await dbService.collection("knuhouse").add({
-      // first record : column name of the data(DB) ------ second record : real data (state)
-      record: record,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
+    // // insert data to database
+    // await dbService.collection("knuhouse").add({
+    //   // first record : column name of the data(DB) ------ second record : real data (state)
+    //   record: record,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
 
-    // flush inputform
-    setRecord("");
+    // // flush inputform
+    // setRecord("");
   };
 
   const handleChange = (e) => {
@@ -44,12 +46,58 @@ const GetRecord = ({ userObj }) => {
     setRecord(value);
   };
 
+  const handleFileChange = (e) => {
+    // List of files
+    // console.log(e.target.files);
+    const {
+      target: { files },
+    } = e;
+    // get 1st file (input file)
+    const file = files[0];
+    console.log(file);
+
+    //  File reader change Rec file
+    const reader = new FileReader();
+
+    // if encoding to URL finish -> onloadend
+    reader.onloadend = (finishedEvent) => {
+      // set record data with result of finishedEvent
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setRecordData(result);
+      console.log(finishedEvent);
+    };
+
+    // read data and make URL
+    reader.readAsDataURL(file);
+  };
+
+  const handleClear = () => {
+    setRecordData(null);
+  };
+
   // console.log(texts);
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} placeholder="">
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="text_placeholder">
+          <input
+            type="file"
+            accept="audio/*"
+            capture
+            onChange={handleFileChange}
+          />
           <input type="submit" value="record" />
+          {recordData && (
+            <>
+              <audio src={recordData} />
+              <button onClick={handleClear}>다시 녹음 버튼</button>
+            </>
+          )}
         </input>
       </form>
       <div>
