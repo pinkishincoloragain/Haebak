1a925db9b79c74837d7d673318e3b54faf706c6e
SeongukBaek <bsu0541@naver.com>
2021-07-21 16:30:07 +0900

    Update Aboutpage

---
+++ client/src/components/AboutPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import BackButton from "./common/BackButton";
+import HelpButton from "./HelpButton";
+
+const useStyles = makeStyles((theme) => ({
+    aboutpageRoot: {
+    //   background: `url(${MypageImage}) center center / cover no-repeat`,
+      backgroundAttachment: "fixed",
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      "& > *": {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
+    },
+    mypagePaper: {
+      width: "500px",
+      height: "725px",
+      border: "3px solid",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "space-around",
+    },
+  }));
+
+const AboutPage = (handleAboutpage) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.aboutpageRoot}>
+      <Paper elevation={3} className={classes.mypagePaper}>
+        <div style={{ width: "100%" }}>
+          <BackButton type="mypage" action={handleAboutpage} />
+        </div>
+        <div style={{ fontSize: "1.6em" }}>
+          <p>WHAT ABOUT?</p>
+        </div>
+      </Paper>
+      <HelpButton />
+    </div>
+  );
+};
+
+export default AboutPage;
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -4,6 +4,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import MainHeader from "../components/MainHeader";
 import MainPaperList from "../components/MainPaperList";
 import MyPage from "../components/MyPage";
+import AboutPage from "../components/AboutPage";
+import HelpButton from "../components/HelpButton";
 import GetRecord from "../components/GetRecord";
 
 const useStyles = makeStyles({
@@ -15,19 +17,23 @@ const useStyles = makeStyles({
 const Main = ({ isLoggedIn, userObj }) => {
   const classes = useStyles();
   const [isMypage, setIsMypage] = useState(false);
+  const [isAbout, setIsAbout] = useState(false);
 
   const handleMypage = () => setIsMypage(!isMypage);
+  const handleAboutpage = () => setIsAbout(!isAbout);
 
   return (
     <div className={classes.mainRoot}>
-      {!isMypage ? (
+      {!isMypage ? (!isAbout ? (
         <div>
-          <MainHeader handleMypage={handleMypage} />
+          <MainHeader handleMypage={handleMypage} handleAboutpage={handleAboutpage}/>
           <MainPaperList />
+          <HelpButton />
         </div>
       ) : (
-        <MyPage userObj={userObj} handleMypage={handleMypage} />
-      )}
+        <AboutPage handleAboutpage={handleAboutpage} />
+      )) : <MyPage userObj={userObj} handleMypage={handleMypage} />
+      }
     </div>
   );
 
