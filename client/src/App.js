345b038e520e42fe95b71d09d32db475a3539887
woong-jae <jwchung0828@gmail.com>
2021-07-23 02:19:18 +0900

    콘솔 로그 제거

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,7 @@ import Landing from "./views/Landing";
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import { authService, dbService } from "./firebase";
-import Activity from "./views/Activity";
 import Pending from "./components/common/Pending";
-import { useReducer } from "react";
-import firebase from "firebase/app";
 
 function App() {
   // initialization for Firebase Firestore
@@ -28,13 +25,11 @@ function App() {
 
     authService.onAuthStateChanged((user) => {
       // user is logged in
-      console.log(user);
       if (user) {
         fetchUserData(user);
         setUserObj(user);
         setIsLoggedIn(true);
-        console.log(user.emailVerified);
-        if (user.emailVerified != true) {
+        if (user.emailVerified !== true) {
           alert("check your email!");
           authService.signOut();
         }
