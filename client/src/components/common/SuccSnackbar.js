1cce954351eea3829d7af181e2c9cb03f2f048a3
SeongukBaek <bsu0541@naver.com>
2021-07-22 23:54:41 +0900

    Update Snackbar

---
+++ client/src/components/common/SuccSnackbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Snackbar} from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+}));
+
+const SuccSnackbar = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          질문 제출이 완료되었습니다!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
+
+export default SuccSnackbar;
